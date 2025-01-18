5db94e67f26829dd90adcd8b41519cbc29d70da1
Egor <dementev.eg@yandex.ru>
2025-01-18 12:41:59 +0300

    create application

---
+++ src/main/java/com/example/photo_analysis/PhotoAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.photo_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAnalysisApplication.class, args);
+	}
+
+}
